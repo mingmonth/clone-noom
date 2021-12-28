3ec03699a09551180ba4788eab174e8b681280ea
yskim <yskim@huinno.com>
2021-12-28 22:57:02 +0900

    #2.1 Installing SocketIO (07:51)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -13,41 +14,46 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  console.log(socket);
+});
+// const wss = new WebSocket.Server({ server });
 
 // function handleConnection(socket) {
 //   console.log(socket);
 // }
 
 // fake database
-const sockets = [];
+// const sockets = [];
 
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log("Connected to Browser");
-  socket.on("close", () => console.log("Disconnected from the Browser!!"));
-  socket.on("message", (msg) => {
-    // console.log(msg.toString("utf8"));
-    // socket.send(msg.toString("utf8"));
-    const message = JSON.parse(msg.toString("utf8"));
-    console.log(message);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}:${message.payload}`)
-        );
-        break;
-      case "nickname":
-        console.log(message.payload);
-        socket["nickname"] = message.payload;
-        break;
-      default:
-        console.log("default");
-    }
-  });
-  //   socket.send("welcome to chat!!");
-});
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anonymous";
+//   console.log("Connected to Browser");
+//   socket.on("close", () => console.log("Disconnected from the Browser!!"));
+//   socket.on("message", (msg) => {
+//     // console.log(msg.toString("utf8"));
+//     // socket.send(msg.toString("utf8"));
+//     const message = JSON.parse(msg.toString("utf8"));
+//     console.log(message);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}:${message.payload}`)
+//         );
+//         break;
+//       case "nickname":
+//         console.log(message.payload);
+//         socket["nickname"] = message.payload;
+//         break;
+//       default:
+//         console.log("default");
+//     }
+//   });
+//   //   socket.send("welcome to chat!!");
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
