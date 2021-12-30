6e7e85f02d4c4b623851c72611e672ae51eb20de
yskim <yskim@huinno.com>
2021-12-31 08:48:32 +0900

    #2.8 Room Count part One (15:23)

--- src/server.js
+++ src/server.js
@@ -17,9 +17,25 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sid, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anonymous";
   socket.onAny((event) => {
+    console.log(wsServer.sockets.adapter);
     console.log(`Socket Event:${event}`);
   });
   socket.on("enter_room", (roomName, done) => {
