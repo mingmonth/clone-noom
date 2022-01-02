90c1a2cc30c7623afcd3f4b97ac19752a7a00ef1
yskim <yskim@huinno.com>
2022-01-02 21:59:40 +0900

    #3.1 Call Controls (10:45)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,19 +3,38 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 
 let myStream;
 let muted;
 let cameraOff;
 
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log(devices);
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    console.log(cameras);
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 async function getMedia() {
   try {
     myStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
+      audio: true,
       video: true,
     });
     console.log(myStream);
     myFace.srcObject = myStream;
+    await getCameras();
   } catch (e) {
     console.log(e);
   }
@@ -24,6 +43,10 @@ async function getMedia() {
 getMedia();
 
 function handleCameraClick() {
+  console.log(myStream.getVideoTracks());
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
@@ -34,6 +57,11 @@ function handleCameraClick() {
 }
 
 function handleMuteClick() {
+  console.log(myStream.getAudioTracks());
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
